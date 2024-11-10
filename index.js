2ad9099f1c6c178c6cc9e4ed403c23a5fc3f6d20
yousof-ali <yousofali293@gmail.com>
2024-11-10 18:54:54 +0600

    create simple server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req,res) => {
+    res.send("Haven server is running!");
+})
+app.listen(port,() => {
+    console.log(`haven is running on port ${port}`);
+})
