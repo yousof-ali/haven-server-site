c07ec9a26e2ba2ce0597f67d2451133b517b0450
yousof-ali <yousofali293@gmail.com>
2024-11-12 16:14:51 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 
@@ -8,6 +9,39 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lewcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(url,{
+    serverApi:{
+        version:ServerApiVersion.v1,
+        strict:true,
+        deprecationErrors:true,
+    }
+})
+
+async function run(){
+    try{
+
+        await client.connect();
+
+        const estateCollections = client.db("haven").collection("homes");
+        const userCollections = client.db("haven").collection("users");
+
+
+        app.get('/homes',async(req,res) => {
+            const cursor = estateCollections.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        console.log('connected with mongodb successfully');
+        
+    }finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/',(req,res) => {
